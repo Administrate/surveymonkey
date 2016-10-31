93f56b79b0a0e30865cb67acd9df9231edc2fd3f
Aaron Bassett <me@aaronbassett.com>
2016-10-31 16:51:24 +0000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requirements = [
 
 setup(
     name='surveymonkey',
-    version='0.1.3',
+    version='0.1.4',
     description="Python wrapper for the Survey Monkey v3 API",
     long_description=readme,
     author="Aaron Bassett",
