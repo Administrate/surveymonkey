e7a74f23d98ea110e7c44c68c52027c249630ae3
Aaron Bassett <me@aaronbassett.com>
2016-11-02 11:32:00 +0000

    Fix bug with URL_COLLECTOR_SINGLE

--- surveymonkey/constants.py
+++ surveymonkey/constants.py
@@ -7,7 +7,7 @@ URL_USER_ME = "%s/users/me" % API_URL
 URL_SURVEYS_LIST = "%s/surveys" % API_URL
 
 URL_COLLECTOR_CREATE = "%s/surveys/{survey_id}/collectors" % API_URL
-URL_COLLECTOR_SINGLE = "%s/collectors" % API_URL
+URL_COLLECTOR_SINGLE = "%s/collectors/{collector_id}" % API_URL
 URL_COLLECTOR_RESPONSES_BULK = "%s/collectors/{collector_id}/responses/bulk" % API_URL
 
 URL_MESSAGE_CREATE = "%s/collectors/{collector_id}/messages" % API_URL
