0a8c80ec37c61e61fc313d01354e7fbf64df61d9
Aaron Bassett <me@aaronbassett.com>
2016-11-03 10:44:24 +0000

    Add responses as well as responses/bulk

--- surveymonkey/collectors/collectors.py
+++ surveymonkey/collectors/collectors.py
@@ -3,7 +3,8 @@
 from furl import furl
 from ..manager import BaseManager
 from ..constants import (URL_COLLECTOR_CREATE, URL_COLLECTOR_RESPONSES_BULK,
-                         URL_SURVEY_RESPONSES_BULK, URL_COLLECTOR_SINGLE)
+                         URL_SURVEY_RESPONSES_BULK, URL_COLLECTOR_SINGLE,
+                         URL_SURVEY_RESPONSES, URL_COLLECTOR_RESPONSES)
 
 
 class Collector(BaseManager):
@@ -34,14 +35,17 @@ class Collector(BaseManager):
         return self.get(url)
 
 
-class CollectorResponsesBulk(BaseManager):
+class CollectorResponsesBase(BaseManager):
+
+    URL_SURVEY_RESPONSES = None
+    URL_COLLECTOR_RESPONSES = None
 
     def __init__(self, connection, collector_ids, survey_id=None):
-        super(CollectorResponsesBulk, self).__init__(connection)
+        super(CollectorResponsesBase, self).__init__(connection)
         self.is_multi = isinstance(collector_ids, list)
         self.collector_ids = collector_ids
         self.survey_id = survey_id
-        self.url = URL_SURVEY_RESPONSES_BULK if self.is_multi else URL_COLLECTOR_RESPONSES_BULK
+        self.url = self.URL_SURVEY_RESPONSES if self.is_multi else self.URL_COLLECTOR_RESPONSES
 
         if self.is_multi and not self.survey_id:
             raise AttributeError("Multiple collectors requested with no survey id")
@@ -54,7 +58,7 @@ class CollectorResponsesBulk(BaseManager):
         else:
             url = url.format(collector_id=self.collector_ids)
 
-        return super(CollectorResponsesBulk, self).build_url(url, *args, **kwargs)
+        return super(CollectorResponsesBase, self).build_url(url, *args, **kwargs)
 
     def responses(self):
         next_url = self.build_url(self.url, page=1)
@@ -79,3 +83,13 @@ class CollectorResponsesBulk(BaseManager):
         url = self.build_url(self.url, page=1)
         next_url = "%s&status=disqualified" % url
         return self.get_list(next_url=next_url)
+
+
+class CollectorResponses(CollectorResponsesBase):
+    URL_SURVEY_RESPONSES = URL_SURVEY_RESPONSES
+    URL_COLLECTOR_RESPONSES = URL_COLLECTOR_RESPONSES
+
+
+class CollectorResponsesBulk(CollectorResponsesBase):
+    URL_SURVEY_RESPONSES = URL_SURVEY_RESPONSES_BULK
+    URL_COLLECTOR_RESPONSES = URL_COLLECTOR_RESPONSES_BULK
