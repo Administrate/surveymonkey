c9a30fe9aa6e686f3b03b31ab277e138d05762cb
Aaron Bassett <me@aaronbassett.com>
2017-01-24 15:41:36 +0000

    Add additional validation for survey responses

--- surveymonkey/tests/responses.py
+++ surveymonkey/tests/responses.py
@@ -4,11 +4,14 @@ from __future__ import absolute_import
 
 import random
 
+import requests
+import pytest
 from httmock import HTTMock
 from expects import expect, have_keys, be_a
 
 from surveymonkey.responses import Response
-from surveymonkey.tests.mocks.responses import ResponseGetMock
+from surveymonkey.exceptions import SurveyMonkeyBadResponse
+from surveymonkey.tests.mocks.responses import ResponseGetMock, ResponseValidationMocks
 from surveymonkey.tests.utils import create_fake_connection
 
 
@@ -30,3 +33,91 @@ class TestGetResponse(object):
 
         expect(response).to(be_a(dict))
         expect(response).to(have_keys('id', 'response_status', 'pages'))
+
+
+class TestResponseSchemaValidation(object):
+
+    def setup_class(self):
+        self.mocks = ResponseValidationMocks()
+
+    def test_malformed_json_raises_bad_response(self):
+        with HTTMock(self.mocks.invalid_json_content):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_json_which_doesnt_match_schema_raises_bad_response(self):
+        with HTTMock(self.mocks.does_not_match_schema):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_invalid_analyze_url_raises_bad_response(self):
+        with HTTMock(self.mocks.invalid_analyze_url):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_invalid_edit_url_raises_bad_response(self):
+        with HTTMock(self.mocks.invalid_edit_url):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_invalid_href_url_raises_bad_response(self):
+        with HTTMock(self.mocks.invalid_href_url):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_response_status_has_invalid_value(self):
+        with HTTMock(self.mocks.response_status_unknown):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_other_answer_does_not_have_text(self):
+        with HTTMock(self.mocks.remove_text_from_answer_type_other):
+            response = requests.get('mocked://')
+            with pytest.raises(SurveyMonkeyBadResponse):
+                Response.verify_response_json(response)
+
+    def test_choice_col_row_is_valid_answer_combination(self):
+        with HTTMock(self.mocks.choice_col_row_answer):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_just_choice_is_valid_answer(self):
+        with HTTMock(self.mocks.choice_only_answer):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_choice_and_row_is_valid_answer_combination(self):
+        with HTTMock(self.mocks.choice_row_answer):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_just_test_is_valid_answer(self):
+        with HTTMock(self.mocks.text_only_answer):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_text_and_row_is_valid_answer(self):
+        with HTTMock(self.mocks.text_and_row_answer):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_text_and_content_type_is_valid_answer(self):
+        with HTTMock(self.mocks.text_and_content_type_answer):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_answer_can_be_empty_dict(self):
+        with HTTMock(self.mocks.answer_is_empty_dict):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
+
+    def test_answers_can_be_multiple(self):
+        with HTTMock(self.mocks.answer_is_multiple):
+            response = requests.get('mocked://')
+            Response.verify_response_json(response)
