f6dae0e257ec5b935086f0c3479f7b6253809b7c
Alec MacQueen <agm@administrate.co>
2018-05-08 09:49:06 +0100

    Update base manager to take access url
    The api url for survey monkey can be different based on where you
    signed up for your account, see:
    https://developer.surveymonkey.com/api/v3/#access-url
    
    This change allows the base manager to take both a connection object with an
    access url and the previous connection value by itself. This allows the
    api url to be overridden when necessary.

--- surveymonkey/tests/test_base_manager.py
+++ surveymonkey/tests/test_base_manager.py
@@ -10,7 +10,7 @@ from expects import expect, have_key, have_keys, equal, be_a, contain
 from mock import MagicMock
 
 from surveymonkey.manager import BaseManager
-from surveymonkey.constants import URL_USER_ME
+from surveymonkey.constants import URL_USER_ME, API_URL
 from surveymonkey.exceptions import SurveyMonkeyBadResponse
 
 from surveymonkey.tests.utils import create_fake_connection
@@ -34,7 +34,7 @@ class TestBaseManager(object):
 
     def test_parse_response_returns_a_dict_with_correct_keys(self):
         with HTTMock(UsersResponseMocks().me):
-            response = self.session.get(URL_USER_ME)
+            response = self.session.get(API_URL + URL_USER_ME)
             data = self.manager.parse_response(response)
 
             expect(data).to(have_keys('id', 'username', 'email'))
@@ -146,3 +146,27 @@ class TestBaseManager(object):
 
         self.manager.set_quotas(response)
         expect(self.manager.quota_exceeded).to(equal(False))
+
+    def test_access_url_overrides_api_url_when_provided(self):
+        connection = MagicMock(
+            connection=self.connection,
+            access_url="http://overridden.com"
+        )
+        manager = BaseManager(connection)
+
+        overriden_url = manager.build_url(URL_USER_ME)
+        expect(overriden_url).to(equal("http://overridden.com" + URL_USER_ME))
+
+    def test_add_base_url_handles_pre_formatted_urls(self):
+        connection = MagicMock(
+            connection=self.connection,
+            access_url="http://overridden.com"
+        )
+        manager = BaseManager(connection)
+        formatted_overridden_url = "http://overridden.com" + URL_USER_ME
+        formatted_default_url = API_URL + URL_USER_ME
+
+        overriden_url = manager.build_url(formatted_overridden_url)
+        default_url = manager.build_url(formatted_default_url)
+        expect(overriden_url).to(equal(formatted_overridden_url))
+        expect(default_url).to(equal(formatted_default_url))
