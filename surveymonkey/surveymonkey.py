e8a90f354849d00db915c5ed6385268302331443
Tommaso D'Odorico <tod@getadministrate.com>
2018-05-22 11:20:17 +0100

    bugfix - Access_url not used correctly

--- surveymonkey/surveymonkey.py
+++ surveymonkey/surveymonkey.py
@@ -2,13 +2,23 @@
 from __future__ import absolute_import
 
 import six
+from furl import furl
+
+from surveymonkey.constants import API_VERSION
 
 
 class SurveyMonkeyConnection(object):
 
     def __init__(self, access_token, access_url=None):
-        self.ACCESS_TOKEN = access_token
-        self.ACCESS_URL = access_url
+        self.access_token = access_token
+        self.access_url = self._add_api_version_to_url(access_url) if access_url else None
+
+    @staticmethod
+    def _add_api_version_to_url(url):
+        url = furl(url)
+        if not url.path or url.path == '/':
+            url.join(API_VERSION)
+        return url.url.rstrip('/')
 
 
 class BaseConfig(object):
