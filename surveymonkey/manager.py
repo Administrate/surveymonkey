7d05c190108328fc544341ba6cf3d2f0415a4407
Tommaso D'Odorico <tod@getadministrate.com>
2018-05-22 11:20:17 +0100

    bugfix - Access_url not used correctly

--- surveymonkey/manager.py
+++ surveymonkey/manager.py
@@ -14,12 +14,12 @@ class BaseManager(object):
 
     def __init__(self, connection):
         self.connection = connection
-        self.access_url = getattr(connection, 'access_url', None)
+        self.access_url = connection.access_url
 
     def create_session(self):
         session = requests.Session()
         session.headers.update({
-            "Authorization": "Bearer %s" % self.connection.ACCESS_TOKEN,
+            "Authorization": "Bearer %s" % self.connection.access_token,
             "Content-Type": "application/json"
         })
 
