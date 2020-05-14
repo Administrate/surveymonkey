334088218eb7f9cace70dd081fbad14e08962606
Luca Gallici <lg@administrate.co>
2020-05-14 14:09:36 +0100

    Bump version: 0.13.3 → 0.14.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='surveymonkey',
-    version='0.13.3',
+    version='0.14.0',
     description="Python wrapper for the Survey Monkey v3 API",
     long_description=readme,
     author="Aaron Bassett",
